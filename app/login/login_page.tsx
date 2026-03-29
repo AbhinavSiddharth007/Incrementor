df4b47e0bc02bbcc39ae8a2b2655c3f982850577
Abhinav <siddharthabhinav74@gmail.com>
2026-03-29 22:11:42 +0200

    first commit

--- app/login/login_page.tsx
+++ app/login/login_page.tsx
@@ -3,9 +3,9 @@ import { useState } from "react";
 export default function login_page(){
     const [count , setCount] = useState(0);
     
-    return <view>
+    return <div>
         <h1>Login Button press counter :D</h1>
         <button onClick={()=> setCount(count+1)}> + </button>
 
-    </view>
+    </div>
 }
